cb70666ba0bb139c75c3d8e7678640664201896c
smcvb <smcvbeelen@gmail.com>
2013-11-21 17:24:30 +0100

    Fine tuned Stepwise Inverted Indexing

--- workspace/Hama Programs/src/InvertedIndex.java
+++ workspace/Hama Programs/src/InvertedIndex.java
@@ -29,11 +29,12 @@ import types.TextLongIntMessage;
  * Hama program to run the Inverted Indexing algorithm as
  *  specified in 'Data-Intensive Text Processing with MapReduce'
  * @author stevenb
- * @date 15-10-2013
+ * @date 21-11-2013
  */
 public class InvertedIndex extends Configured implements Tool {
 	
 	public static final int BSP_TASKS = 37; // 26 letters + 10 digits + 1 signs
+	public static final int MAX_PAGES_READ = 50000; // The number of pages to parse before performing a superstep
 	public static final String[] STOPWORDS = {"a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at",
 			"be", "because", "been", "before", "being", "below", "between", "both", "but", "by",
 			"can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during",
@@ -55,22 +56,21 @@ public class InvertedIndex extends Configured implements Tool {
 	
 	public static class InvertedIndexBSP extends BSP<TextLongPair, IntWritable, Text, TextIntPairArrayWritable, TextLongIntMessage> {
 		
-		private String currentTerm, previousTerm;
-		private HashMap<String, Integer> postingTupleMap;
+		private int maxPagesParsed;
 		private HashSet<String> stopwordSet;
-		private ArrayList<TextIntPair> postingsList;
+		private HashMap<String, Integer> postingTupleMap;
+		private HashMap<String, ArrayList<TextIntPair>> invertedIndex;
 		private TextIntPairArrayWritable writablePostings;
 		
 		@Override
 		public void setup(BSPPeer<TextLongPair, IntWritable, Text, TextIntPairArrayWritable, TextLongIntMessage> peer) throws IOException { //initialize
-			currentTerm = "[empty]";
-			previousTerm = null;
-			postingTupleMap = new HashMap<String, Integer>();
+			maxPagesParsed = peer.getConfiguration().getInt("maxPagesParsed", 0);
 			stopwordSet = new HashSet<String>();
 			for (String stopword : STOPWORDS) {
 				stopwordSet.add(stopword);
 			}
-			postingsList = new ArrayList<TextIntPair>();
+			postingTupleMap = new HashMap<String, Integer>();
+			invertedIndex = new HashMap<String, ArrayList<TextIntPair>>();
 			writablePostings = new TextIntPairArrayWritable(TextIntPair.class);
 		}
 		
@@ -84,9 +84,12 @@ public class InvertedIndex extends Configured implements Tool {
 		 */
 		@Override
 		public void bsp(BSPPeer<TextLongPair, IntWritable, Text, TextIntPairArrayWritable, TextLongIntMessage> peer) throws IOException, InterruptedException, SyncException {
-			createTermFrequencies(peer);
-			peer.sync();
-			createPostingsList(peer);
+			boolean finished = false;
+			while(!finished){
+				finished = createTermFrequencies(peer);
+				peer.sync();
+				createPostingsList(peer);
+			}
 		}
 		
 		/**
@@ -96,7 +99,8 @@ public class InvertedIndex extends Configured implements Tool {
 		 * @throws IOException for the reading the read values 
 		 * 		and sending messages to other peers
 		 */
-		private void createTermFrequencies(BSPPeer<TextLongPair, IntWritable, Text, TextIntPairArrayWritable, TextLongIntMessage> peer) throws IOException {
+		private boolean createTermFrequencies(BSPPeer<TextLongPair, IntWritable, Text, TextIntPairArrayWritable, TextLongIntMessage> peer) throws IOException {
+			int pagesParsed = 0;
 			TextLongPair key = new TextLongPair();
 			IntWritable value = new IntWritable();
 			
@@ -117,7 +121,13 @@ public class InvertedIndex extends Configured implements Tool {
 					peer.send(other, tuple);
 				}
 				postingTupleMap.clear(); // Empty memory
+				
+				pagesParsed++;
+				if(pagesParsed >= maxPagesParsed){
+					return false;
+				}
 			}
+			return true;
 		}
 		
 		/**
@@ -128,32 +138,39 @@ public class InvertedIndex extends Configured implements Tool {
 		 * 		and for writing the output
 		 */
 		private void createPostingsList(BSPPeer<TextLongPair, IntWritable, Text, TextIntPairArrayWritable, TextLongIntMessage> peer) throws IOException {
-			int totalMessages = peer.getNumCurrentMessages();
+			TextLongIntMessage tuple;
 			
-			for (int i = 0; i < totalMessages; i++) {
-				TextLongIntMessage tuple = peer.getCurrentMessage();
-				
-				currentTerm = tuple.getTerm().toString();
-				if (!currentTerm.equals(previousTerm) && previousTerm != null) { // New term start, write out old term
-					TextIntPair[] postingsArray = new TextIntPair[postingsList.size()];
-					postingsArray = postingsList.toArray(postingsArray);
-					writablePostings.set(postingsArray);
-					peer.write(new Text(previousTerm), writablePostings);
-					postingsList.clear(); // Empty memory
-				}
+			while ((tuple = peer.getCurrentMessage()) != null) {
+				String term = tuple.getTerm().toString();
+				ArrayList<TextIntPair> postingsList = (invertedIndex.containsKey(term) ? invertedIndex.get(term) : new ArrayList<TextIntPair>());
 				TextIntPair posting = new TextIntPair(new Text(tuple.getDocid().toString()), new IntWritable(tuple.getFrequency().get()));
 				postingsList.add(posting);
-				previousTerm = currentTerm;
+				
+				invertedIndex.remove(term);
+				invertedIndex.put(term, postingsList);
 			}
 		}
 		
+		/** 
+		 * Write all term, postings list pairs to output
+		 * @param peer: BSPPeer object containing all the information of this peer
+		 * @throws IOException for write()
+		 */
 		@Override
 		public void cleanup(BSPPeer<TextLongPair, IntWritable, Text, TextIntPairArrayWritable, TextLongIntMessage> peer) throws IOException { // Close
-			TextIntPair[] postingsArray = new TextIntPair[postingsList.size()];
-			postingsArray = postingsList.toArray(postingsArray);
-			writablePostings.set(postingsArray);
-			peer.write(new Text(previousTerm), writablePostings);
-			postingsList.clear(); // Empty memory
+			String term;
+			ArrayList<TextIntPair> postingsList;
+			
+			for(Entry<String, ArrayList<TextIntPair>> entry : invertedIndex.entrySet()) {
+				term = entry.getKey();
+				postingsList = entry.getValue();
+				
+				TextIntPair[] postingsArray = new TextIntPair[postingsList.size()];
+				postingsArray = postingsList.toArray(postingsArray);
+				writablePostings.set(postingsArray);
+				peer.write(new Text(term), writablePostings);
+				postingsList.clear(); // Empty memory
+			}
 		}
 	}
 	
@@ -164,12 +181,13 @@ public class InvertedIndex extends Configured implements Tool {
 	 * @return a finalized BSPJob Object for this BSP job
 	 * @throws IOException for creating the BSP job Object
 	 */
-	public static BSPJob createJob(HamaConfiguration conf, String inputPath, String outputPath, int tasks) throws IOException {
+	public static BSPJob createJob(HamaConfiguration conf, String inputPath, String outputPath, int tasks, int maxPagesParsed) throws IOException {
 		conf.set("wiki.language", "en");
+		conf.setInt("maxPagesParsed", maxPagesParsed);
 		conf.set(MessageManager.QUEUE_TYPE_CLASS, "org.apache.hama.bsp.message.queue.SortedMessageQueue"); // Internal Sorting as needed, hence sorted message queue
 		
 		BSPJob job = new BSPJob(conf, InvertedIndex.class); // Main settings
-		job.setJobName("Inverted Indexing");
+		job.setJobName("Inverted Indexing Step");
 		job.setBspClass(InvertedIndexBSP.class);
 		job.setNumBspTask(tasks);
 		job.setInputPath(new Path(inputPath)); // Input settings
@@ -185,7 +203,7 @@ public class InvertedIndex extends Configured implements Tool {
 	}
 	
 	private int printUsage() {
-		System.out.println("usage:\t <input path> <output path> <number of tasks [default = 37]>");
+		System.out.println("usage:\t <input path> <output path> <number of tasks [default = 37]> <number of files to read before synchronizing [default = 50000]>");
 		ToolRunner.printGenericCommandUsage(System.out);
 		return -1;
 	}
@@ -199,7 +217,8 @@ public class InvertedIndex extends Configured implements Tool {
 	 * @throws Exception from the createJob() and the waitForCompletion() methods
 	 */
 	public int run(String[] args) throws Exception {
-		int tasks = 0;
+		int tasks = 0, 
+				maxPagesParsed = 0; // The number of pages to parse before performing a superstep
 		String inputPath = "", outputPath = "";
 		HamaConfiguration conf = new HamaConfiguration(getConf());
 		
@@ -210,19 +229,29 @@ public class InvertedIndex extends Configured implements Tool {
 		}
 		inputPath = args[0];
 		outputPath = args[1];
-		if (args.length >= 3) {
+		if (args.length >= 4) {
+			try {
+				tasks = Integer.parseInt(args[2]);
+				maxPagesParsed = Integer.parseInt(args[3]);
+			} catch (NumberFormatException e) {
+				System.err.println("Error: expected Integers instead of " + args[2] + " and " + args[3]);
+				return printUsage();
+			}
+		} else if (args.length >= 3) {
 			try {
 				tasks = Integer.parseInt(args[2]);
 			} catch (NumberFormatException e) {
 				System.err.println("Error: expected Integer instead of " + args[2]);
 				return printUsage();
 			}
+			maxPagesParsed = MAX_PAGES_READ;
 		} else {
 			tasks = BSP_TASKS;
+			maxPagesParsed = MAX_PAGES_READ;
 		}
 		
 		// Create and start a job
-		BSPJob job = createJob(conf, inputPath, outputPath, tasks);
+		BSPJob job = createJob(conf, inputPath, outputPath, tasks, maxPagesParsed);
 		long startTime = System.currentTimeMillis();
 		if (job.waitForCompletion(true)) {
 			System.out.println("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
